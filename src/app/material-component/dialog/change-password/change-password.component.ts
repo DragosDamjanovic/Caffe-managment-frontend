7c239f25c2519658ea6f1a10a8e114c4184f9a5d
dragosDamjanovic <dragos@infopuls.net>
2023-05-19 22:04:20 +0200

    Initial commit

---
+++ src/app/material-component/dialog/change-password/change-password.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { UserService } from 'src/app/services/user.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss']
+})
+export class ChangePasswordComponent implements OnInit {
+  changePasswordForm: any = FormGroup;
+  respnseMessage: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private dialogRef: MatDialogRef<ChangePasswordComponent>,
+    private ngxService: NgxUiLoaderService,
+    private snackbarService: SnackbarService
+  ) { }
+
+  ngOnInit(): void {
+    this.changePasswordForm = this.formBuilder.group({
+      oldPassword: [null, [Validators.required]],
+      newPassword: [null, [Validators.required]],
+      confrimPassword: [null, [Validators.required]]
+    })
+  }
+
+  validateSubmit() {
+    if (this.changePasswordForm.controls['newPassword'].value != this.changePasswordForm.controls['confrimPassword'].value) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  handleChangePasswordSubmit() {
+    this.ngxService.start();
+    var formData = this.changePasswordForm.value;
+    var data = {
+      oldPassword: formData.oldPassword,
+      newPassword: formData.newPassword,
+      confirmPassword: formData.confirmPassword
+    }
+    this.userService.changePassword(data).subscribe((response: any) => {
+      this.ngxService.stop();
+      this.respnseMessage = response?.message;
+      this.dialogRef.close();
+      this.snackbarService.openSnackBar(this.respnseMessage, "success");
+    }, (error) => {
+      console.log(error);
+      this.ngxService.stop();
+      if (error.error?.message) {
+        this.respnseMessage = error.error?.message;
+      } else {
+        this.respnseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.respnseMessage, GlobalConstants.error);
+    })
+  }
+
+}
